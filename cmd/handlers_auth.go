c06d0dc24aa23d98bc7ee3a44826511d7024916d
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-06-10 14:55:05 +0200

    Parse body of POST request to /auth/register

--- cmd/handlers_auth.go
+++ cmd/handlers_auth.go
@@ -3,10 +3,25 @@ package main
 
 import (
 	"net/http"
+	"encoding/json"
 	"log"
+	"fmt"
 )
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string	`json:"password"`
+}
 
 func (s *Server) handleRegister(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%s %s\n", r.Method, r.URL.Path)
-	w.WriteHeader(http.StatusOK)
+	var credentials Credentials
+	if err := json.NewDecoder(r.Body).Decode(&credentials); err != nil {
+		log.Println("error:", err.Error())
+		http.Error(w, "invalid body", http.StatusBadRequest)
+		return
+	}
+	fmt.Println(credentials.Username)
+	fmt.Println(credentials.Password)
+	w.WriteHeader(http.StatusCreated)
 }
