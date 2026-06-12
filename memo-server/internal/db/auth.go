a2aed23835689f2413e13c288da25051012df71f
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-06-12 15:06:39 +0200

    Hash password

--- memo-server/internal/db/auth.go
+++ memo-server/internal/db/auth.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"golang.org/x/crypto/bcrypt"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -13,12 +14,24 @@ type Credentials struct {
 }
 
 func CreateUser(ctx context.Context, pool *pgxpool.Pool, credentials Credentials) error {
-	_, err := pool.Exec(ctx, `
+	hashedPassword, err := hashPassword(credentials.Password)
+	if err != nil {
+		return fmt.Errorf("CreateUser: %w", err)
+	}
+	_, err = pool.Exec(ctx, `
 		INSERT INTO users (username, password)
 		VALUES ($1, $2)
-	`, credentials.Username, credentials.Password)
+	`, credentials.Username, string(hashedPassword))
 	if err != nil {
 		return fmt.Errorf("CreateUser: %w", err)
 	}
 	return nil
+}
+
+func hashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("hashPassword: %w", err)
+	}
+	return string(hashedPassword), nil
 }
