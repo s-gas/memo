77f3efc4b17798b35675375e6b0ba8e352aa7328
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-06 18:19:22 +0200

    Create controller directory for routers

--- api/app.js
+++ api/app.js
@@ -1,29 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const registerRouter = require('./controllers/register');
 
-//middlewares
 app.use(cors());
 app.use(express.json());
-
-let users = [];
-
-app.post('/v1/auth/register', (req, res) => {
-  const newUser = req.body;
-  if (!newUser.username || !newUser.email || !newUser.password) {
-    res.status(400).json({error: "All fields are required"});
-    return;
-  }
-  if (users.find((user) => user.email === newUser.email)) {
-    res.status(409).json({error: "Email already in use"});
-    return;
-  }
-  if (users.find((user) => user.username === newUser.username)) {
-    res.status(409).json({error: "Username already exists"});
-    return;
-  }
-  users = users.concat(newUser);
-  res.json(req.body);
-});
+app.use('/v1/auth/register', registerRouter);
 
 module.exports = app;
---
+++ api/controllers/register.js
@@ -0,0 +1,23 @@
+const registerRouter = require('express').Router()
+
+let users = [];
+
+registerRouter.post('/', (req, res) => {
+  const newUser = req.body;
+  if (!newUser.username || !newUser.email || !newUser.password) {
+    res.status(400).json({error: "All fields are required"});
+    return;
+  }
+  if (users.find((user) => user.email === newUser.email)) {
+    res.status(409).json({error: "Email already in use"});
+    return;
+  }
+  if (users.find((user) => user.username === newUser.username)) {
+    res.status(409).json({error: "Username already exists"});
+    return;
+  }
+  users = users.concat(newUser);
+  res.json(req.body);
+});
+
+module.exports = registerRouter
