361ae6d6215c873f479f04803353c8edf20b892f
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-10 14:49:19 +0200

    Handle 409 conflict

--- api/utils/middleware.js
+++ api/utils/middleware.js
@@ -1,6 +1,9 @@
 const handleErrors = (err, req, res, next) => {
   if (err.name === "CastError" || err.name === "ValidationError") return res.status(400).json(err.name);
-  
+  if (err.code === 11000 && err.keyValue) {
+    const keys = Object.keys(err.keyValue);
+    return res.status(409).json({err: `${keys[0]} already exists`})
+  }
   next(err);
 }
 
