756d5075fbcd2ce679236a16ed28270dae4f317f
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-11 11:14:16 +0200

    Modify db schema to store id and delte _id and __v

--- api/models/user.js
+++ api/models/user.js
@@ -18,6 +18,14 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User
