481f352645215417c1039874b293159f8f9cb7c8
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-16 09:41:55 +0200

    Install JWT and read SECRET from .env

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const registerRouter = require('./controllers/register');
+const loginRouter = require('./controllers/login');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const handleErrors = require('./utils/middleware');
@@ -20,7 +21,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use('/v1/auth/register', registerRouter);
+app.use('/v1/auth/login', loginRouter);
 
 app.use(handleErrors);
 
--- api/utils/config.js
+++ api/utils/config.js
@@ -2,5 +2,6 @@ require('dotenv').config();
 
 const PORT = process.env.PORT;
 const MONGODB_URL = process.env.MONGODB_URL;
+const SECRET = process.env.SECRET;
 
-module.exports = { PORT, MONGODB_URL }
+module.exports = { PORT, MONGODB_URL, SECRET }
